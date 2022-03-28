f2daccc7a288cca5fbc15f9e95c8dea1e405a86d
Elias Valter <evalter1612@gmail.com>
2022-03-28 02:04:43 +0200

    java doc update

--- src/main/java/CustomerData/CustomerDataApplication.java
+++ src/main/java/CustomerData/CustomerDataApplication.java
@@ -3,9 +3,21 @@ package CustomerData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+		/**
+		 * @author elias_valter
+		 * @version 1.0
+		 * @since 28th March 2022
+		 */
+
+
 @SpringBootApplication
 public class CustomerDataApplication {
 
+	/**
+	 * main entrance to the program
+	 * @param args arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerDataApplication.class, args);
 	}
